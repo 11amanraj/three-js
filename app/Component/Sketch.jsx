37f6272bd8962c9503f2b416edc1a10568dce573
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-06-30 19:37:54 +0530

    Minor Changes

--- app/Component/Sketch.jsx
+++ app/Component/Sketch.jsx
@@ -9,9 +9,9 @@ const Sketch = () => {
         <mesh>
             <boxGeometry args={[2, 2, 2]} />
             <meshStandardMaterial />
-            <ambientLight intensity={0.1} />
-            <directionalLight color="red" position={[0, 0, 5]} />
         </mesh>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
     </Canvas>
   )
 }
