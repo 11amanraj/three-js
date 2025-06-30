3ac26aff23fbcd96beea51fd65c4057358238dba
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-06-30 20:00:31 +0530

    Added animation

--- app/Component/Sketch.jsx
+++ app/Component/Sketch.jsx
@@ -1,15 +1,26 @@
 'use client'
 
-import React from 'react'
-import { Canvas } from '@react-three/fiber'
+import React, { useRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
 
-const Sketch = () => {
-  return (
-    <Canvas className='bg-amber-400'>
-        <mesh>
+const Box = () => {
+    const meshRef = useRef()
+    
+    useFrame((state, delta) => (meshRef.current.rotation.x += delta))
+    
+    return (
+        <mesh ref={meshRef}>
             <boxGeometry args={[3, 3, 3]} />
             <meshStandardMaterial />
         </mesh>
+    )
+}
+
+const Sketch = () => {
+
+  return (
+    <Canvas className='bg-amber-400'>
+        <Box />
         <ambientLight intensity={0.1} />
         <directionalLight color="red" position={[0, 0, 5]} />
     </Canvas>
